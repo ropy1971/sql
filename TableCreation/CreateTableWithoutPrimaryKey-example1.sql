d3f535e836d9790b4f1d9bdce01c23c3f6889e0f
ropy1971 <rodolphe@petrowsky.fr>
2019-01-24 17:28:33 +0100

    Initial commit

---
+++ TableCreation/CreateTableWithoutPrimaryKey-example1.sql
@@ -0,0 +1,9 @@
+USE [azure-db]
+CREATE TABLE WindowsServer
+(
+    Region [NVARCHAR](30), 
+    Publisher [NVARCHAR](50),
+    Offer [NVARCHAR](50),
+    Sku [NVARCHAR](50)
+);
+GO
---
+++ TableCreation/CreateTableWithoutPrimaryKey-example2.sql
@@ -0,0 +1,9 @@
+USE [azure-db]
+CREATE TABLE regions
+(
+    Location [NVARCHAR](30), 
+    DisplayName [NVARCHAR](50),
+    City [NVARCHAR](50),
+    Country [NVARCHAR](30)
+);
+GO
